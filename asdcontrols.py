b2babfdaca826ee4cf24fb6d8d59f4bd77238c86
Kathleen <kathleenhoza@gmail.com>
2018-07-16 18:06:07 -0700

    reads commands from file names instead of inside of file. Files in C:\Kathleen

--- asdcontrols.py
+++ asdcontrols.py
@@ -53,57 +53,81 @@ class ViewSpecProController:
     def __init__(self, running=False):
         self.app=Application()
         if running:
-            app=Application().connect(path=r"C:\Program Files\ASD\ViewSpecPro\ViewSpecPro.exe")
+            self.app=Application().connect(path=r"C:\Program Files\ASD\ViewSpecPro\ViewSpecPro.exe")
         else:
-            app=Application().start("C:\Program Files\ASD\ViewSpecPro\ViewSpecPro.exe")
-        self.spec=app['ViewSpec Pro    Version 6.2'] 
-        self.pid=app.process
+            self.app=Application().start("C:\Program Files\ASD\ViewSpecPro\ViewSpecPro.exe")
+        self.spec=self.app['ViewSpec Pro    Version 6.2'] 
+        self.pid=self.app.process
     
     def process(self, input_path, output_path, tsv_name):
         self.open_files(input_path)
         self.set_save_directory(output_path)
         self.splice_correction()
         self.ascii_export(output_path, tsv_name)
+        self.spec.menu_select('File -> Close')
+        # if path_el[0]=='C:'or path_el[0]=='c:':
+        #     os.system('dir')
     
     def open_files(self, path):
-        self.app=Application().connect(path=r"C:\Program Files\ASD\ViewSpecPro\ViewSpecPro.exe")
         self.spec.menu_select('File -> Open')
         open=self.app['Select Input File(s)']
         open.set_focus()
-        open.ToolBar2.double_click()
+        open['Address Band Root'].toolbar.button(0).click()
+        #open['Address Band Root'].edit.set_edit_text(path)
         keyboard.SendKeys(path)
+        open['Address Band Root'].edit.set_focus()
         keyboard.SendKeys('{ENTER}')
-        open.directUIHWND.ShellView.click_input()
+        
+        #Make sure *.0** files are visible instead of just *.asd. Note that this won't work if you have over 100 files!!
+        open.ComboBox2.select(1).click()
+        open.ComboBox2.select(1).click()
+        open.directUIHWND.ShellView.set_focus()
         keyboard.SendKeys('^a')
         keyboard.SendKeys('{ENTER}')
     
     def set_save_directory(self,path):
+        time.sleep(2)
         self.spec.menu_select('Setup -> Output Directory')
         save=self.app['New Directory Path']
-        for el in path_el:
-            if el=='C:': el='C:\\'
-            save.ListBox.select(el)
-            select_item(save.ListBox.rectangle())
+        path_el=path.split('\\')
+        if path_el[0]=='C:'or path_el[0]=='c:':
+            for el in path_el:
+                if el=='C:': el='C:\\'
+                save.ListBox.select(el)
+                self.select_item(save.ListBox.rectangle())
+        else:
+            print('Invalid directory (must save to C drive)')
+            # path_el=['C:\\','Users','RS3Admin','Temp']
+            # for el in path_el:
+            #     save.ListBox.select(el)
+            #     self.select_item(save.ListBox.rectangle())
+        save.OKButton.click()
+        #If a dialog box comes up asking if you want to set the default input directory the same as the output, click no. Not sure if there is a different dialog box that could come up, so this doesn't seem very robust.
+        try:
+            self.app['Dialog'].Button2.click()
+        except:
+            pass
+        
     
     def splice_correction(self):
         self.select_all()
-        spec.menu_select('Process -> Splice Correction')
-        app['Splice Correct Gap'].set_focus()
-        app['Splice Correct Gap'].button1.click_input()
-        app['ViewSpecPro'].set_focus()
-        app['ViewSpecPro'].button1.click_input()
+        self.spec.menu_select('Process -> Splice Correction')
+        self.app['Splice Correct Gap'].set_focus()
+        self.app['Splice Correct Gap'].button1.click_input()
+        self.app['ViewSpecPro'].set_focus()
+        self.app['ViewSpecPro'].button1.click_input()
         
     def ascii_export(self, path, tsv_name):
-        select_all(self.spec)
+        self.select_all()
         self.spec.menu_select('Process -> ASCII Export')
-        export=app['ASCII Export']
+        export=self.app['ASCII Export']
         export.ReflectanceRadioButton.check()
         export.AbsoluteCheckBox.check()
         export.OutputToASingleFileCheckBox.check()
         export.set_focus()
         export.Button2.click_input()
         
-        save=app['Select Ascii File']
+        save=self.app['Select Ascii File']
         save.set_focus()
         save.ToolBar2.double_click()
         keyboard.SendKeys(path)
@@ -111,8 +135,7 @@ class ViewSpecProController:
         save.edit.set_edit_text(tsv_name)
         save.set_focus()
         save.OKButton.click_input()
-        app['ViewSpecPro'].set_focus()
-        app['ViewSpecPro'].button1.click_input()
+        self.app['Dialog'].OKButton.click()
         
         
     def select_all(self):
@@ -129,7 +152,7 @@ class ViewSpecProController:
                     spec=self.app[title]#=Application().connect(handle=el.handle)
         return spec
         
-    def select_item(rectangle):
+    def select_item(self,rectangle):
         #set start position at center top of listbox
         im=pyautogui.screenshot()
         x=rectangle.left+0.5*(rectangle.right-rectangle.left)
--- listener.py
+++ listener.py
@@ -7,9 +7,13 @@ dev=True
 RS3_running=True
 ViewSpecPro_running=True
 
-if dev: sys.path.append('c:/users/rs3admin/hozak/python/autospec/')
+if dev: 
+    sys.path.append('c:/users/rs3admin/hozak/python/autospec/')
+    #os.system('del C:\\Kathleen\commands\*')
 
 import asdcontrols
+
+share_loc='C:\Kathleen'
 
 if dev:
     imp.reload(asdcontrols)
@@ -17,13 +21,17 @@ if dev:
     from asdcontrols import ViewSpecProController
 
 def main():
+    delme=os.listdir(share_loc+'/commands/')
+    for file in delme:
+        cmd='del C:\\Kathleen\\commands\\'+file
+        os.system(cmd)
     spec_controller=RS3Controller(running=RS3_running)
     process_controller=ViewSpecProController(running=ViewSpecPro_running)
     
-    cmds0=os.listdir('//PATATO/spec_share/commands/')
+    cmds0=os.listdir(share_loc+'/commands/')
     while True:
         
-        cmds=os.listdir('//PATATO/spec_share/commands/')
+        cmds=os.listdir(share_loc+'/commands/')
         if cmds==cmds0:
             print('no change')
         else:
@@ -34,10 +42,16 @@ def main():
                     elif 'wr' in cmd: spec_controller.white_reference()
                     elif 'opt' in cmd: spec_controller.optimize()
                     elif 'process' in cmd:
-                        file=open('//PATATO/spec_share/commands/'+cmd,'r')
-                        input_path=file.readline()
-                        output_path=file.readline()
-                        tsv_name=file.readline()
+                        params=cmd.split('_')
+                        input_path=params[2].replace('-','\\')
+                        input_path=input_path.replace('&',':')
+                        output_path=params[3].replace('-','\\')
+                        output_path=output_path.replace('&',':')
+                        tsv_name=params[4]
+                        #file=open(share_loc+'/commands/'+cmd,'r')
+                        # input_path=file.readline().strip('\n')
+                        # output_path=file.readline().strip('\n')
+                        # tsv_name=file.readline().strip('\n')
                         process_controller.process(input_path, output_path, tsv_name)
             cmds0=cmds
         time.sleep(1)
